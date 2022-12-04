cac24ea35161bec96e6c913c3512a3772398d9b6
FelipeShinjiAriji <felipe.ariji@gmail.com>
2022-12-04 19:06:23 -0300

    14° Commit: physicalMemory
    Implements physicalMemory, with LRU and FIFO algorithms

--- Code/Main.py
+++ Code/Main.py
@@ -1,13 +1,14 @@
 import sys
 from Functions import extractDataFromArchive, relocate
 
-address = sys.argv[1]
-quadro = sys.argv[2]
-algoritimo = sys.argv[3]
+address = 'C:\\Users\\Usuario\\Documents\\SistemasOperacionais\\Prova\\Prova2\\ArquivoLivro2\\addresses.txt' # sys.argv[1]
+quadro = 128  # sys.argv[2]
+algoritimo = 'LRU'  # sys.argv[3]
 
 
 tlb = []
 pageTable = []
+physicalMemory = []
 
 
 def processInputCommands(inputCommand: int):
@@ -44,7 +45,9 @@ def processLogicAddress(logicAddress: int):
     pageNumber = logicAddress//256
     for pageFramePair in tlb:
         if pageNumber == pageFramePair[0]:  # TLB hit
-            if algoritimo == 'LRU':
+            if algoritimo == 'LRU': # LRU in TLB and in physical memory
+                physicalMemory.remove(pageFramePair[1])
+                physicalMemory.append(pageFramePair[1])
                 tlb.remove(pageFramePair)
                 tlb.append(pageFramePair)
             return pageFramePair[1]  # return frame
@@ -52,6 +55,14 @@ def processLogicAddress(logicAddress: int):
         if not (pageNumber in pageTable):
             pageTable.append(pageNumber)
         frameNumber = pageTable.index(pageNumber)
+
+        if algoritimo == 'LRU': #LRU in physical memory
+            for physicalFrame in physicalMemory:
+                if physicalFrame == frameNumber:
+                    physicalMemory.remove(physicalFrame)
+                    physicalMemory.append(physicalFrame)
+
+        relocate(frameNumber ,physicalMemory, quadro)
         relocate([pageNumber, frameNumber], tlb, 16)
         return frameNumber
 
